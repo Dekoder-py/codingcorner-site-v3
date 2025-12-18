b9c258f95ac4e495af3381d0d87d916bde5f076a
Dekoder-py <dekod3r.py@gmail.com>
2025-12-18 21:46:35 +1300

    add greeting (yes i borrowed this idea from <@U091KE59H5H>!)

---
+++ index.js
@@ -0,0 +1,18 @@
+// Greeting
+const greetingElement = document.getElementById("greeting");
+const currentHour = new Date().getHours();
+let greetingText = "Hello!";
+
+if (currentHour < 6) {
+  greetingText = "You're up early! (or are you up very late...)";
+} else if (currentHour < 12) {
+  greetingText = "Good morning!";
+} else if (currentHour < 17) {
+  greetingText = "Good afternoon!";
+} else if (currentHour < 22) {
+  greetingText = "Good evening!";
+} else {
+  greetingText = "Still awake?";
+}
+
+greetingElement.textContent = greetingText;
