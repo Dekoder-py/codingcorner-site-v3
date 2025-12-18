dbfa1c38e006ca143d0b977d52edab0ff8692f4b
Dekoder-py <dekod3r.py@gmail.com>
2025-12-18 21:55:49 +1300

    update greetings

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ if (currentHour < 6) {
   greetingText = "You're up early! (or are you up very late...)";
 } else if (currentHour < 12) {
   greetingText = "Good morning!";
-} else if (currentHour < 17) {
+} else if (currentHour <= 17) {
   greetingText = "Good afternoon!";
-} else if (currentHour < 22) {
+} else if (currentHour <= 22) {
   greetingText = "Good evening!";
 } else {
   greetingText = "Still awake?";
